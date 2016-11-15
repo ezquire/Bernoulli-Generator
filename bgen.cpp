088052d7e82075634ff63a3915c5859b3fcf88b9
ezquire <tyler.gearing@gmail.com>
2016-11-15 12:40:33 -0800

    set up funciton prototype

--- bgen.cpp
+++ bgen.cpp
@@ -7,18 +7,40 @@
 
 using namespace std;
 
+/***********FUNCTION PROTOTYPES********/
 
+void bgen(const char*, int, int, float, int, const char*);
 
-int bernoulli(int numberOfThrows, double bias)
+/***********DECLARED VARIABLES***********/
+
+//Variable for number of tosses the user would like to see.
+const char* GEN_MODE = "gen";
+const char* STAT_MODE = "stat"
+int numberOfExperiments = 0;
+int numberOfTrials = 0;
+float probabilityBias = 0.0;
+int generator = 0;  //Selecting 0, 1, 2 selects from three different random generators
+const char* filename = "output.txt"
+
+// Variable for selecting which side of the coin gets more bias
+// where 1 > bias and 0 < bias. Bias is initialized to 0.5 to indicated an unbiased coin.
+double bias = 0.5;
+
+//Boolean variable for selecting the mode. Default is "0" or "stat mode".
+
+int main() {
+    bgen("gen", 100000, 10, 0.1, "output.txt");
+}
+
+void bgen(const char* mode, int N, int T, float P, const char* filename)
 {
     std::random_device rd;
     std::mt19937 gen(rd());
-    // give "true" based on percent bias
-    // give "false" based on percent bias
-    std::bernoulli_distribution d(bias);
+
+    std::bernoulli_distribution d(P);
 
     std::map<bool, int> hist;
-    for(int n=0; n<numberOfThrows; ++n) {
+    for(int n=0; n<N; ++n) {
         ++hist[d(gen)];
     }
     for(auto p : hist) {
@@ -27,7 +49,7 @@ int bernoulli(int numberOfThrows, double bias)
     }
 }
 
-int main()
+int randomizer()
 {
     // Seed with a real random value, if available
     std::random_device r;
@@ -51,5 +73,17 @@ int main()
     for (auto p : hist) {
         std::cout << std::fixed << std::setprecision(1) << std::setw(2)
                   << p.first << ' ' << std::string(p.second/200, '*') << '\n';
+    }
+}
+
+bool getGenerator() {
+    int mode = 0;
+    cout << "Pick one";
+    cin >> mode;
+    switch(mode) {
+        case 1 :
+            return 1;
+        case 0 :
+            return 0;
     }
 }
